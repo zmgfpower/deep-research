b51c1ec709b4bbed23fd8a18cd0025d9d3faaf3b
Amery2010 <amery@xiangfa.org>
2025-03-17 11:46:21 +0800

    build: Restore rewrites, preserving the same request logic as before

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 import pkg from "./package.json";
 
 const BUILD_MODE = process.env.NEXT_PUBLIC_BUILD_MODE;
+const API_PROXY_BASE_URL =
+  process.env.API_PROXY_BASE_URL || "https://generativelanguage.googleapis.com";
+const GOOGLE_GENERATIVE_AI_API_KEY = process.env
+  .GOOGLE_GENERATIVE_AI_API_KEY as string;
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -19,6 +23,22 @@ if (BUILD_MODE === "export") {
   nextConfig.basePath = "";
 } else if (BUILD_MODE === "standalone") {
   nextConfig.output = "standalone";
+} else {
+  nextConfig.rewrites = async () => {
+    return [
+      {
+        source: "/api/ai/google/v1beta/:path*",
+        has: [
+          {
+            type: "header",
+            key: "x-goog-api-key",
+            value: "(?<key>.*)",
+          },
+        ],
+        destination: `${API_PROXY_BASE_URL}/v1beta/:path*?key=${GOOGLE_GENERATIVE_AI_API_KEY}`,
+      },
+    ];
+  };
 }
 
 export default nextConfig;
